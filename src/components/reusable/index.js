6dd659ca3e83a69271be43ce69274df5b605646d
MatoJob <mato.jobbagy@gmail.com>
2017-07-13 20:24:43 +0000

    Revert "Merge branch 'feature/RM-4' into 'master'"
    This reverts merge request !3

--- src/components/reusable/index.js
+++
@@ -1,7 +0,0 @@
-export * from './Button';
-export * from './Card';
-export * from './CardItem';
-export * from './Divider';
-export * from './SectionHeader';
-export * from './MatchItem';
-export * from './Input';
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import routes from './routes';
-// ... other reducers
-
-export default combineReducers({
-  routes,
-  // ... other reducers
-});
