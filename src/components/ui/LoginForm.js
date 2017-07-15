6e393b8acd46fdec63413e3368e3de7da066acb0
Martin Jobbagy <martin.jobbagy@globallogic.com>
2017-07-15 13:39:20 +0200

    firebase repaired

--- src/components/ui/LoginForm.js
+++ src/components/ui/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-//import firabase from 'firabase';
+//import { View, Text } from 'react-native';
+//import firebase from 'firebase';
 import { Card, CardItem, Button, Input, Divider } from '../reusable';
 
 
